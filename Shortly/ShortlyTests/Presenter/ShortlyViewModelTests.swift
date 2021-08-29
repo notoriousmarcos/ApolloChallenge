c520038b671ac419775e3d708b52069c6b22d83a
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-08-29 11:25:13 -0300

    ShortlyViewModel remove shortly

--- Shortly/ShortlyTests/Presenter/ShortlyViewModelTests.swift
+++ Shortly/ShortlyTests/Presenter/ShortlyViewModelTests.swift
@@ -11,21 +11,37 @@ import Combine
 
 class ShortlyViewModel: ObservableObject, ShortlyViewModelProtocol {
 
-    @Published public var shortlyURLs: [ShortlyURLViewModel] = []
+    @Published public var shortlyURLViewModels: [ShortlyURLViewModel] = []
 
+    private var shortURLs: [ShortlyURLModel] = []
     private var fetchAllURLs: FetchAllAction
+    private var removeURL: RemoveAction
 
-    init(fetchAllURLs: @escaping FetchAllAction) {
+    init(fetchAllURLs: @escaping FetchAllAction,
+         removeURL: @escaping RemoveAction) {
         self.fetchAllURLs = fetchAllURLs
+        self.removeURL = removeURL
     }
 
     public func onAppear() {
         self.getAllURLs()
     }
 
+    func deleteURL(_ viewModel: ShortlyURLViewModel) {
+        guard let model = shortURLs.first(where: { $0.code == viewModel.code }) else {
+            return
+        }
+        removeURL(model) { [weak self] success in
+            if success {
+                self?.getAllURLs()
+            }
+        }
+    }
+
     private func getAllURLs() {
         self.fetchAllURLs { [weak self] urls in
-            self?.shortlyURLs = urls.compactMap {
+            self?.shortURLs = urls
+            self?.shortlyURLViewModels = urls.compactMap {
                 ShortlyURLViewModel(code: $0.code,
                                     shortLink: $0.shortLink,
                                     originalLink: $0.originalLink)
@@ -35,9 +51,11 @@ class ShortlyViewModel: ObservableObject, ShortlyViewModelProtocol {
 }
 
 protocol ShortlyViewModelProtocol {
+    typealias RemoveAction = (ShortlyURLModel, (Bool) -> Void) -> Void
     typealias FetchAllAction = (([ShortlyURLModel]) -> Void) -> Void
 
     func onAppear()
+    func deleteURL(_ model: ShortlyURLViewModel)
 }
 
 class ShortlyViewModelTests: XCTestCase {
@@ -57,17 +75,49 @@ class ShortlyViewModelTests: XCTestCase {
                                         shareLink: "shrtco.de/share/KCveN",
                                         fullShareLink: "https://shrtco.de/share/KCveN",
                                         originalLink: "http://example.org/very/long/link.html")])
-        })
+        }, removeURL: { _,_  in })
 
         // Act
         sut.onAppear()
 
-        _ = sut.$shortlyURLs.sink { urls in
+        _ = sut.$shortlyURLViewModels.sink { urls in
             XCTAssertEqual(urls.count, 1)
         }
 
         // Assert
         XCTAssertEqual(fetchAllCalledCount, 1)
+    }
 
+    func testShortlyViewModel_remove_shouldReturnAllURLsToView() {
+        var allURLs = [ShortlyURLModel(code: "KCveN",
+                                       shortLink: "shrtco.de/KCveN",
+                                       fullShortLink: "https://shrtco.de/KCveN",
+                                       shortLink2: "9qr.de/KCveN",
+                                       fullShortLink2: "https://9qr.de/KCveN",
+                                       shareLink: "shrtco.de/share/KCveN",
+                                       fullShareLink: "https://shrtco.de/share/KCveN",
+                                       originalLink: "http://example.org/very/long/link.html")]
+        var fetchAllCalledCount = 0
+        let sut = ShortlyViewModel(fetchAllURLs: { completion in
+            fetchAllCalledCount += 1
+            completion(allURLs)
+        }, removeURL: { model, completion in
+            XCTAssertEqual(model.code, "KCveN")
+            allURLs = []
+            completion(true)
+        })
+
+        // Act
+        sut.onAppear()
+        sut.deleteURL(ShortlyURLViewModel(code: "KCveN",
+                                          shortLink: "shrtco.de/KCveN",
+                                          originalLink: "http://example.org/very/long/link.html"))
+
+        _ = sut.$shortlyURLViewModels.sink { urls in
+            XCTAssertEqual(urls.count, 0)
+        }
+
+        // Assert
+        XCTAssertEqual(fetchAllCalledCount, 2)
     }
 }
